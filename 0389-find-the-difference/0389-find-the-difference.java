83114c7308917c3f0a5c23a904c746b1489ca9c5
Keerthana Mani <keerthanam.23csd@kongu.edu>
2026-07-02 19:28:48 +0530

    Time: 12 ms (11.6%), Space: 43.4 MB (19.85%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,22 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // Count characters in s
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        // Check characters in t
+        for (char ch : t.toCharArray()) {
+            if (!map.containsKey(ch) || map.get(ch) == 0) {
+                return ch;
+            }
+
+            map.put(ch, map.get(ch) - 1);
+        }
+
+        return ' ';   // This line will never be reached for valid inputs.
+    }
+}
