8e23d0412b0864b80f74f6a5c12fe93932f380cd
Keerthana Mani <keerthanam.23csd@kongu.edu>
2026-08-17 20:05:39 +0530

    Time: 2 ms (9.89%), Space: 43.3 MB (6.22%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+        String[] s1 = s.trim().split("\\s+");
+
+        int n = s1.length;
+
+        return s1[n - 1].length();
+    }
+}
