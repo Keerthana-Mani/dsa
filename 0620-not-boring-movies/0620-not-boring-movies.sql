2e48a60a797702abe1f0bc742a07a234d2b8c3b3
Keerthana Mani <keerthanam.23csd@kongu.edu>
2026-08-24 18:29:47 +0530

    Time: 289 ms (49.53%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,1 +1,1 @@
-select * from Cinema where id%2=1 and description!='Boring' order by rating DESC;
+select id,movie,description,rating from Cinema where id % 2 =1 and description != 'boring'order by rating desc;
