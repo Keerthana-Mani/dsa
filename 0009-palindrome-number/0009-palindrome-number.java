a9443650daa5bdf0d64bf73c1786f5745b0fd38c
Keerthana Mani <keerthanam.23csd@kongu.edu>
2026-09-15 14:53:54 +0530

    Time: 5 ms (82.88%), Space: 46 MB (33.04%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0;
+        int o=x;
+        while(x>0){
+            int d=x%10;
+            rev=rev*10+d;
+            x=x/10;
+        }
+        if(o==rev){
+            return true;
+        }
+        return false;
+    }
+}
