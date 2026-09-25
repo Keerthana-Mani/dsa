d6e29c1fb9188342bcc76336d06e9b4560871dd3
Keerthana Mani <keerthanam.23csd@kongu.edu>
2026-09-25 09:28:49 +0530

    Time: 1 ms (84.01%), Space: 42.6 MB (86.89%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,22 +1,22 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         HashMap<Character,String>map=new HashMap<>();
-        String[]s1=s.split(" ");
+        String []s1=s.split(" ");
+        int i=0;
         if(pattern.length()!=s1.length){
             return false;
-        }int i=0;
+        }
         for(char ch:pattern.toCharArray()){
             if(map.containsKey(ch)){
                 if(!map.get(ch).equals(s1[i])){
                     return false;
                 }
             }
-            else{
-                if (map.containsValue(s1[i])) {
-               return false;
-            }
-                map.put(ch,s1[i]);
-            }
+           else {
+    if (map.containsValue(s1[i])) {
+        return false;
+    }
+}   map.put(ch,s1[i]);
             i++;
         }
         return true;
